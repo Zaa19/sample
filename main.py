6f5374ffff7647db1f90d50017121676eb270c66
Zaa19 <69797005+Zaa19@users.noreply.github.com>
2020-08-18 13:04:00 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+
+name = input("enter your name :")
+age = input("enter your age: ")
+
+print("your name is " + name + " and your age is " + age)
+
+number = input("enter you favourite number:")
+
+print ("ok i give you " + number + " books")
+
